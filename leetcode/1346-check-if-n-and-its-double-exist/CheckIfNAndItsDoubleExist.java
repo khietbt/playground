066ca7c0670dc6815a6817a019710adf805dcdc1
khietbt <khietbt@gmail.com>
2024-12-02 10:15:34 +0700

    Update Java solution of leetcode's 1346th problem

--- leetcode/1346-check-if-n-and-its-double-exist/CheckIfNAndItsDoubleExist.java
+++ leetcode/1346-check-if-n-and-its-double-exist/CheckIfNAndItsDoubleExist.java
@@ -3,17 +3,13 @@ class Solution {
         int[] cache = new int[2000 * 2 + 1];
 
         for (int i = 0; i < a.length; i++) {
-            cache[a[i] + 1000] = i + 1;
-        }
+            // int currentValue = a[i];
 
-        for (int i = 0; i < a.length; i++) {
-            int doubleValue = a[i] * 2;
-
-            int index = cache[doubleValue + 1000];
-
-            if (index != 0 && index != i + 1) {
+            if (cache[a[i] * 2 + 2000] != 0 || (a[i] % 2 == 0 && cache[a[i] / 2 + 2000] != 0)) {
                 return true;
             }
+
+            cache[a[i] + 2000] = i + 1;
         }
 
         return false;
@@ -23,8 +19,8 @@ class Solution {
 public class CheckIfNAndItsDoubleExist {
     public static void main(String[] arguments) {
         // int[] a = { 0, 0 };
-        int[] a = { 10, 2, 5, 3 };
-        // int[] a = { -1, 2, 0, -4 };
+        // int[] a = { 10, 2, -5, 3 };
+        int[] a = { -1, 2, 0, -4 };
 
         System.out.println((new Solution()).checkIfExist(a));
     }
