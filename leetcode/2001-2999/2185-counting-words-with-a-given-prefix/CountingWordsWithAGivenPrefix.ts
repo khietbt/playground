722ee016bb9528be8b2562261a729a826b2eea74
khietbt <khietbt@gmail.com>
2024-12-13 14:52:16 +0700

    Add a Java solution for 2185 Counting Words With a Given Prefix

--- leetcode/2001-2999/2185-counting-words-with-a-given-prefix/CountingWordsWithAGivenPrefix.ts
+++ leetcode/2001-2999/2185-counting-words-with-a-given-prefix/CountingWordsWithAGivenPrefix.ts
@@ -1,8 +1,5 @@
 function prefixCount(words: string[], pref: string): number {
-    return words.filter((w) => w.indexOf(pref) === 0).length;
+  return words.filter((w) => w.indexOf(pref) === 0).length;
 }
 
-const words = ["pay","attention","practice","attend"];
-const pref = "at";
-
-console.log(prefixCount(words, pref));
+console.log(prefixCount(["pay", "attention", "practice", "attend"], "at"));
