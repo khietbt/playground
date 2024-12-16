ccc1b3aaf09f96fbebcc51756e535a48c934c565
Khiet Bui <khietbt@gmail.com>
2024-12-16 04:05:05 +0000

    Add a Java solution to leetcode's 3264th problem

---
+++ leetcode/3001-4000/3264-final-array-state-after-k-multiplication-operations-i/FinalArrayStateAfterKMultiplicationOperationsI.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class FinalArrayStateAfterKMultiplicationOperationsI {
+  public static void main(String[] args) {
+    Solution s = new Solution();
+
+    System.out.println(Arrays.toString(s.getFinalState(new int[] { 1, 2 }, 3, 4)));
+  }
+}
+
+class Solution {
+  public int[] getFinalState(int[] numbers, int k, int multiplier) {
+    PriorityQueue<Integer> queue = new PriorityQueue<>(
+        (a, b) -> numbers[a] != numbers[b] ? numbers[a] - numbers[b] : a - b);
+
+    for (int i = 0; i < numbers.length; i++) {
+      queue.offer(i);
+    }
+
+    while (k-- > 0) {
+      int index = queue.poll();
+
+      numbers[index] *= multiplier;
+
+      queue.offer(index);
+
+      System.out.println(Arrays.toString(numbers));
+    }
+
+    return numbers;
+  }
+}
