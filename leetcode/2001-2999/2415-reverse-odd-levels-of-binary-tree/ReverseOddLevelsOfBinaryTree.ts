e71519fbac9fc7457de7dca0861ea88c98e7ef07
khietbt <khietbt@gmail.com>
2024-12-23 23:52:14 +0700

    Update the Typescript solution of leetcode's 2415th problem

--- leetcode/2001-2999/2415-reverse-odd-levels-of-binary-tree/ReverseOddLevelsOfBinaryTree.ts
+++ leetcode/2001-2999/2415-reverse-odd-levels-of-binary-tree/ReverseOddLevelsOfBinaryTree.ts
@@ -7,6 +7,91 @@ class TreeNode {
     this.left = (left === undefined ? null : left)
     this.right = (right === undefined ? null : right)
   }
+
+  static from(values: number[]): TreeNode | null {
+    const nodes: TreeNode[] = [];
+
+    if (values.length == 0) {
+      return null;
+    }
+
+    const root = new TreeNode(values.shift());
+    nodes.push(root);
+
+    while (nodes.length > 0) {
+      const node = nodes.shift();
+
+      if (node === undefined) {
+        continue;
+      }
+
+      if (values.length > 0) {
+        const left = new TreeNode(values.shift());
+
+        node.left = left;
+
+        nodes.push(left);
+      }
+
+      if (values.length > 0) {
+        const right = new TreeNode(values.shift());
+
+        node.right = right;
+
+        nodes.push(right);
+      }
+    }
+
+    return root;
+  }
+
+  static print(root: TreeNode | null): void {
+    if (root === null) return;
+
+    const nodes: TreeNode[] = [root];
+
+    let i = 0;
+
+    while (i < nodes.length) {
+      const node = nodes[i];
+
+      if (node.left != null) {
+        nodes.push(node.left);
+      }
+
+      if (node.right != null) {
+        nodes.push(node.right);
+      }
+
+      i++;
+    }
+
+    console.log(nodes.map(node => node.val));
+  }
+}
+
+
+const values: number[] = [];
+
+for (let i = 0; i < 31; i++) {
+  values.push(i);
+}
+
+const root: TreeNode | null = TreeNode.from(values);
+
+const getDepth = (node: TreeNode | null): number => node == null ? 0 : (getDepth(node.left) + 1);
+
+function swap(left: TreeNode, right: TreeNode, depth: number) {
+  if (depth % 2 == 0) {
+    const t = left.val;
+    left.val = right.val;
+    right.val = t;
+  }
+
+  if (depth > 1) {
+    swap(left.left!, right.right!, depth - 1);
+    swap(left.right!, right.left!, depth - 1)
+  }
 }
 
 function reverseOddLevels(root: TreeNode | null): TreeNode | null {
@@ -14,36 +99,14 @@ function reverseOddLevels(root: TreeNode | null): TreeNode | null {
     return null;
   }
 
-  const nodes: TreeNode[] = [];
+  const depth = getDepth(root);
 
-  nodes.push(root);
-
-  let level = 0;
-  let totalElements = 1 << level;
-  let mid = totalElements / 2;
-
-  while (nodes.length > 0) {
-    if (level % 2 == 1) {
-      for (let i = mid; i < totalElements; i++) {
-        const t = nodes[i].val;
-        nodes[i].val = nodes[mid - (i - mid) - 1].val;
-        nodes[totalElements - i - 1].val = t;
-      }
-    }
-
-    for (let i = 0; i < totalElements; i++) {
-      let node = nodes.shift();
-
-      if (node && node.left && node?.right) {
-        nodes.push(node.left);
-        nodes.push(node?.right);
-      }
-    }
-
-    level++;
-    totalElements = 1 << level;
-    mid = totalElements >> 1;
+  if (depth > 1) {
+    swap(root.left!, root.right!, depth - 1);
   }
 
   return root;
 };
+
+reverseOddLevels(root);
+TreeNode.print(root);
