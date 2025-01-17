ce155c38e0371816f2791a2884d141aaa7144350
khietbt <khietbt@gmail.com>
2025-01-17 11:04:05 +0700

    Fix boundary cases in the Java solution

--- leetcode/1001-2000/1301-1400/1137-n-th-tribonacci-number/NThTribonacciNumber.java
+++ leetcode/1001-2000/1301-1400/1137-n-th-tribonacci-number/NThTribonacciNumber.java
@@ -6,6 +6,14 @@ public class NThTribonacciNumber {
 
 class Solution {
   public int tribonacci(int n) {
+    if (n == 0) {
+      return 0;
+    } else if (n == 1) {
+      return 1;
+    } else if (n == 2) {
+      return 1;
+    }
+
     int t0 = 0;
     int t1 = 1;
     int t2 = 1;
