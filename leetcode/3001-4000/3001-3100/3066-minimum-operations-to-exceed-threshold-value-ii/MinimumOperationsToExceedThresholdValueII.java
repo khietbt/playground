754892e78067f0e9b4a7eaf601d3da36e4f98e30
khietbt <khietbt@gmail.com>
2025-02-13 13:20:18 +0700

    Add a Java solution to leetcode's 3066th problem

--- leetcode/3001-4000/3001-3100/3066-minimum-operations-to-exceed-threshold-value-ii/MinimumOperationsToExceedThresholdValueII.java
+++ leetcode/3001-4000/3001-3100/3066-minimum-operations-to-exceed-threshold-value-ii/MinimumOperationsToExceedThresholdValueII.java
@@ -2,8 +2,14 @@ import java.util.PriorityQueue;
 
 public class MinimumOperationsToExceedThresholdValueII {
   public static void main(String[] args) {
-    int[] nums = { 2, 11, 10, 1, 3 };
-    int k = 10;
+    // int[] nums = { 2, 11, 10, 1, 3 };
+    // int k = 10;
+    //
+    // int[] nums = { 1, 1, 2, 4, 9 };
+    // int k = 20;
+
+    int[] nums = { 999999999, 999999999, 999999999 };
+    int k = 1000000000;
 
     System.out.println(new Solution().minOperations(nums, k));
   }
@@ -13,23 +19,29 @@ class Solution {
   public int minOperations(int[] nums, int k) {
     int operations = 0;
 
-    PriorityQueue<Integer> queue = new PriorityQueue<>();
+    PriorityQueue<Long> queue = new PriorityQueue<>();
 
-    for (int num : nums) {
+    for (long num : nums) {
       queue.offer(num);
     }
 
-    while (queue.size() >= 2) {
-      int x = queue.poll();
-      int y = queue.poll();
-
-      if (x > k) {
+    while (!queue.isEmpty()) {
+      // System.out.println(Arrays.toString(queue.toArray(Long[]::new)));
+      if (queue.peek() >= k) {
         return operations;
       }
 
+      if (queue.size() < 2) {
+        break;
+      }
+
+      Long x = queue.poll();
+      Long y = queue.poll();
+
       operations++;
 
-      queue.offer(Math.min(x, y) * Math.max(x, y));
+      queue.offer(Math.min(x, y) * 2 + Math.max(x, y));
+      // System.out.println(Arrays.toString(queue.toArray(Long[]::new)));
     }
 
     return -1;
