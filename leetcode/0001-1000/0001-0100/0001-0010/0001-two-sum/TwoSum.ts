3c92d07f7cdd8ff1fed3169b1a5d8298f32de91c
khietbt <khietbt@gmail.com>
2025-07-09 00:39:32 +0700

    Simplify the Typescript solution to leetcode's 1st problem

--- leetcode/0001-1000/0001-0100/0001-0010/0001-two-sum/TwoSum.ts
+++ leetcode/0001-1000/0001-0100/0001-0010/0001-two-sum/TwoSum.ts
@@ -1,23 +1,18 @@
 function twoSum(numbers: number[], target: number): number[] {
-    const cached: Record<number, number> = {};
+  const cached: Record<number, number> = {};
 
-    for (let i = 0; i < numbers.length; i++) {
-        const complement = target - numbers[i];
+  for (let i = 0; i < numbers.length; i++) {
+    const lhs = numbers[i];
+    const rhs = target - lhs;
 
-        // Check if the complement exists in the cache
-        // If it does, return the indices of the two numbers
-        // that add up to the target
-        if (cached[complement] !== undefined) {
-            return [cached[complement], i];
-        }
-
-        // Store the index of the current number in the cache
-        // to check for its complement in future iterations
-        cached[numbers[i]] = i;
+    if (cached[rhs] !== undefined) {
+      return [cached[rhs], i];
     }
 
-    // If no solution is found, return an empty array
-    return [];
+    cached[lhs] = i;
+  }
+
+  return [];
 };
 
 console.log(twoSum([2, 7, 11, 15], 9)); // Output: [0, 1]
