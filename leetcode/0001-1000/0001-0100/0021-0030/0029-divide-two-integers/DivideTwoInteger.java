2ab88a7f1692ef84cd0a006b4339cd39c9d34755
khietbt <khietbt@gmail.com>
2026-01-09 13:15:20 +0700

    Add a Java solution to leetcode's  29th problem.

--- leetcode/0001-1000/0001-0100/0021-0030/0029-divide-two-integers/DivideTwoInteger.java
+++ leetcode/0001-1000/0001-0100/0021-0030/0029-divide-two-integers/DivideTwoInteger.java
@@ -1,3 +1,5 @@
+import java.io.Console;
+
 public class DivideTwoInteger {
 
   public static void main(String[] args) {
@@ -5,20 +7,47 @@ public class DivideTwoInteger {
 
     System.out.println(solution.divide(10, 3));
     System.out.println(solution.divide(7, -3));
+    System.out.println(solution.divide(-2147483648, 1));
+    System.out.println(solution.divide(-2147483648, 2));
   }
 }
 
 class Solution {
   public int divide(int dividend, int divisor) {
+    if (divisor == 1) {
+      return dividend;
+    }
+
+    if (dividend == 0) {
+      return 0;
+    }
+
+    if (dividend == Integer.MIN_VALUE && divisor == -1) {
+      return Integer.MAX_VALUE;
+    }
+
     var sign = (dividend > 0) == (divisor > 0);
     var quotient = 0;
 
-    dividend = Math.abs(dividend);
-    divisor = Math.abs(divisor);
+    if (dividend > 0) {
+      dividend = -dividend;
+    }
 
-    while (dividend > divisor) {
-      dividend -= divisor;
-      quotient++;
+    if (divisor > 0) {
+      divisor = -divisor;
+    }
+
+    while (dividend <= divisor) {
+      var currentQuotient = 1;
+      var currentDivisor = divisor;
+
+      while ((currentDivisor >= (Integer.MIN_VALUE >> 1)) && (currentDivisor << 1) >= dividend) {
+        currentQuotient <<= 1;
+        currentDivisor <<= 1;
+      }
+
+      dividend -= currentDivisor;
+      quotient += currentQuotient;
     }
 
     return sign ? quotient : -quotient;
