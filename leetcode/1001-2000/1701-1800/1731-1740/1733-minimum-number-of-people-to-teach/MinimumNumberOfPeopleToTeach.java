7376c103bd869a3011712df92b7398175bbf09f5
khietbt <khietbt@gmail.com>
2026-02-06 17:24:33 +0700

    Add a Java solution to leetcode's 1733rd problem.

---
+++ leetcode/1001-2000/1701-1800/1731-1740/1733-minimum-number-of-people-to-teach/MinimumNumberOfPeopleToTeach.java
@@ -0,0 +1,74 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public class MinimumNumberOfPeopleToTeach {
+
+  public static void main(String[] args) {
+    var solution = new Solution();
+
+    // System.out.println(
+    // solution.minimumTeachings(
+    // 2, new int[][] { { 1 }, { 2 }, { 1, 2 } },
+    // new int[][] { { 1, 2 }, { 1, 3 }, { 2, 3 } }));
+
+    System.out.println(
+        solution.minimumTeachings(
+            5, new int[][] {
+                { 1 }, { 5 }, { 1, 5 }, { 5 }
+            },
+            new int[][] {
+                { 1, 2 }, { 1, 3 }, { 1, 4 }, { 2, 3 }
+            }));
+  }
+}
+
+class Solution {
+  public int minimumTeachings(int n, int[][] languages, int[][] friendships) {
+    var users = new HashSet<>();
+
+    var data = new HashMap<Integer, Set<Integer>>();
+
+    for (var i = 0; i < languages.length; i++) {
+      var s = new HashSet<Integer>();
+      for (var l : languages[i]) {
+        s.add(l);
+      }
+
+      data.put(i, s);
+    }
+
+    for (var f : friendships) {
+      var first = f[0] - 1;
+      var second = f[1] - 1;
+
+      if (data.get(first).stream().anyMatch(l -> data.get(second).contains(l))) {
+        continue;
+      }
+
+      users.add(first);
+      users.add(second);
+    }
+
+    // System.out.println(users.toString());
+
+    var minimum = Integer.MAX_VALUE;
+
+    for (var l = 1; l <= n; l++) {
+      var current = users.size();
+
+      for (var u : users) {
+        if (data.get(u).contains(l)) {
+          current--;
+        }
+      }
+
+      System.out.println("l = " + l + ", current = " + current);
+
+      minimum = Math.min(minimum, current);
+    }
+
+    return minimum;
+  }
+}
