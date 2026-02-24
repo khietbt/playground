d5ddbc60c6948575fe266d618b4d3df81e604ff8
khietbt <khietbt@gmail.com>
2026-02-24 16:34:03 +0700

    Add a Java solution to leetcode's 201st problem.

---
+++ leetcode/0001-1000/0201-0300/0201-0210/0201-bitwise-and-of-numbers-range/BitwiseAndOfNumbersRange.java
@@ -0,0 +1,13 @@
+class Solution {
+  public int rangeBitwiseAnd(int left, int right) {
+    var shift = 0;
+    while (left < right) {
+      left >>= 1;
+      right >>= 1;
+
+      shift++;
+    }
+
+    return left << shift;
+  }
+}
