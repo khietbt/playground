4bb2abd05c322c0300f90697d1ca0f64623f12f7
khietbt <khietbt@gmail.com>
2026-04-20 16:12:28 +0700

    Add a README.md to leetcode's 20th problem.

--- leetcode/0001-1000/0001-0100/0081-0090/0088-merge-sorted-array/MergeSortedArray.java
+++ leetcode/0001-1000/0001-0100/0081-0090/0088-merge-sorted-array/MergeSortedArray.java
@@ -1,14 +1,12 @@
-import java.util.Arrays;
-
 public class MergeSortedArray {
 
   public static void main(String[] args) {
     var solution = new Solution();
 
-    // solution.merge(
-    // new int[] { 1, 2, 3, 0, 0, 0 }, 3, new int[] { 2, 5, 6 }, 3);
-    //
-    // solution.merge(new int[] { 1 }, 1, new int[0], 0);
+    solution.merge(
+        new int[] { 1, 2, 3, 0, 0, 0 }, 3, new int[] { 2, 5, 6 }, 3);
+
+    solution.merge(new int[] { 1 }, 1, new int[0], 0);
     solution.merge(new int[] { 0 }, 0, new int[] { 1 }, 1);
   }
 }
