ed81619543a280719cd50f11565e931868e43a08
khietbt <khietbt@gmail.com>
2026-05-02 16:38:53 +0700

    Add a Java solution to leetcode's 117th problem.

--- leetcode/0001-1000/0101-0200/0111-0120/0117-populating-next-right-pointers-in-each-node-ii/PopulatingNextRightPointersInEachNodeII.java
+++ leetcode/0001-1000/0101-0200/0111-0120/0117-populating-next-right-pointers-in-each-node-ii/PopulatingNextRightPointersInEachNodeII.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class PopulatingNextRightPointersInEachNode {
+public class PopulatingNextRightPointersInEachNodeII {
 
   public static void main(String[] args) {
     System.out.println();
