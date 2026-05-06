326b30c455b93e823c0545fd2e57afcd1ae94c7c
khietbt <khietbt@gmail.com>
2026-05-06 15:08:18 +0700

    Add a Java solution to leetcode's 94th problem.

---
+++ leetcode/0001-1000/0001-0100/0091-0100/0094-binary-tree-inorder-traversal/BinaryTreeInorderTreeTraversal.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode() {
+  }
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class Solution {
+  public List<Integer> inorderTraversal(TreeNode root) {
+    if (root == null) {
+      return new ArrayList<>();
+    }
+
+    var stack = new Stack<TreeNode>();
+    var node = root;
+
+    var result = new ArrayList<Integer>();
+
+    while (node != null || !stack.isEmpty()) {
+      while (node != null) {
+        stack.push(node);
+
+        node = node.left;
+      }
+
+      node = stack.pop();
+
+      result.add(node.val);
+
+      node = node.right;
+    }
+
+    return result;
+  }
+}
