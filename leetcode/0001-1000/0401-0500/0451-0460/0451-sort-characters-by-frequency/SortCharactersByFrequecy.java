bda3742add198227e621565ffe1cb3e4fa4db57b
khietbt <khietbt@gmail.com>
2026-05-13 17:53:58 +0700

    Add a Java solution to leetcode's 451st problem.

---
+++ leetcode/0001-1000/0401-0500/0451-0460/0451-sort-characters-by-frequency/SortCharactersByFrequecy.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class SortCharactersByFrequecy {
+
+  public static void main(String[] args) {
+    var solution = new Solution();
+
+    System.out.println(solution.frequencySort("tree"));
+  }
+}
+
+class Solution {
+  public String frequencySort(String s) {
+    var characters = new int[256];
+
+    for (var i = 0; i < s.length(); i++) {
+      characters[s.charAt(i)]++;
+    }
+
+    var queue = new PriorityQueue<Character>((a, b) -> {
+      return characters[b] - characters[a];
+    });
+
+    for (var i = 0; i < 256; i++) {
+      if (characters[i] > 0) {
+        queue.offer((char) i);
+      }
+    }
+
+    var sb = new StringBuilder();
+
+    while (!queue.isEmpty()) {
+      var c = (Character) queue.poll();
+
+      sb.append(String.valueOf(c).repeat(characters[c]));
+    }
+
+    return sb.toString();
+  }
+}
